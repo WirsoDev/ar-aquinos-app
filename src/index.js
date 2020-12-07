b4ff0d260bc718997dfab7ac8d69d7212a1f6937
Wilson Marques <wilson.wmarques@gmail.com>
2020-12-07 11:55:32 +0000

    build base server with dynamic routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const server = express()
+const { modelsinfo } = require('./modelsinfo')
 
-//set up
+
+//set up server
 server.use(express.static('public'))
 
 //template engine
@@ -13,9 +15,30 @@ nunjucks.configure('src/views', {
 
 
 //routs
-
 server.get('/', (req, res)=>{
-    res.render('index.html', {name: 'Hello'})
+    res.render('index.html')
+})
+
+server.get('/:name', (req, res)=>{
+
+    //build path for 3d model by param name
+    var name = req.params.name
+    var modelInfo = modelsinfo[name]
+    var path = modelInfo.devpath
+    var pathios = modelInfo.devpathios
+    var description = modelInfo.description
+
+    //console.log(modelsinfo)
+    res.render('models.html', 
+    {
+        name: name, 
+        path: path, 
+        pathios: pathios,
+        desc: description
+
+    }
+    
+    )
 })
 
 
---
+++ src/modelsinfo.js
@@ -0,0 +1,21 @@
+exports.modelsinfo ={
+    'audrey':{
+        'id': 1030,
+        'element':'3 places sofa',
+        'description':'Audrey sofa',
+        'devpath':'/assets/3dmodels/audrey.glb',
+        'devpathios':'/assets/3dmodels/audreyios.usdz',
+        'path':'',
+        'pathios':''
+    },
+
+    'yoru':{
+        'id': 1030,
+        'element':'3 places sofa',
+        'description':'yuro sofa',
+        'devpath':'/assets/3dmodels/audrey.glb',
+        'devpathios':'/assets/3dmodels/audreyios.usdz',
+        'path':'',
+        'pathios':''
+    }
+}
