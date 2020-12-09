34779d461537940cde496e737b82142c9ef4ff63
Wilson Marques <wilson.wmarques@gmail.com>
2020-12-09 14:31:28 +0000

    remake styles - flexbox

--- public/scripts/colors.js
+++ public/scripts/colors.js
@@ -1,7 +1,7 @@
 
 // set background color by model
 const color = document.querySelector('#modelcolor').innerHTML
-const background = document.querySelector('.app-model-details')
+const background = document.querySelector('.info')
 
 if(color){
     background.style.background = color
