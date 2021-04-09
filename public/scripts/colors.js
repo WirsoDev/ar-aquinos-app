4e51341f472bb6eaa174744a5e61bfec6900570a
Wilson Marques <wilson.wmarques@gmail.com>
2021-04-09 17:23:01 +0100

    add model yoru -- no model link

--- public/scripts/colors.js
+++ public/scripts/colors.js
@@ -2,7 +2,12 @@
 // set background color by model
 const color = document.querySelector('#modelcolor').innerHTML
 const background = document.querySelector('.info')
+const btn = document.querySelector('#btnexemple')
+
 
 if(color){
     background.style.background = color
+    btn.style.background = color
 }
+
+
